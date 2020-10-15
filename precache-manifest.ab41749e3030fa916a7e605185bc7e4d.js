5c4546f0b7d027895f757a97c8b414a9786853c9
dinesh196 <chavandinesh196@gmail.com>
2020-10-15 11:05:24 +0530

    Updates

---
+++ precache-manifest.ab41749e3030fa916a7e605185bc7e4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99b6d9eafe55a191a11c11e0ebc6ec3",
+    "url": "/Amazon_Clone/index.html"
+  },
+  {
+    "revision": "29e58791727631cfacce",
+    "url": "/Amazon_Clone/static/css/main.fb5d88e8.chunk.css"
+  },
+  {
+    "revision": "03ebd9de2672aa8d9317",
+    "url": "/Amazon_Clone/static/js/2.28cf9aea.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/Amazon_Clone/static/js/2.28cf9aea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29e58791727631cfacce",
+    "url": "/Amazon_Clone/static/js/main.0c7ab2e2.chunk.js"
+  },
+  {
+    "revision": "76ae7f5ef1054bacd894",
+    "url": "/Amazon_Clone/static/js/runtime-main.dda3e315.js"
+  }
+]);
